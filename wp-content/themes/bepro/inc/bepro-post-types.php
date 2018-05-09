b5430b463d73863e95b9ff01ea64a9355c121d05
Timofey <tshevchenko@ro.ru>
2018-05-09 13:27:12 +0300

    'Output-Data-From-Our-Advantages'

--- wp-content/themes/bepro/inc/bepro-post-types.php
+++ wp-content/themes/bepro/inc/bepro-post-types.php
@@ -1,6 +1,10 @@
 <?php
-add_action('init', 'bepro_register_post_types');
-function bepro_register_post_types(){
+
+/*
+ * Register Custom Post Type Slider Header Home
+ */
+add_action('init', 'bepro_register_post_type_slider_header_home');
+function bepro_register_post_type_slider_header_home(){
     register_post_type('slider-header-home', array(
         'label'  => null,
         'labels' => array(
@@ -17,23 +21,39 @@ function bepro_register_post_types(){
             'parent_item_colon'  => '', // для родителей (у древовидных типов)
             'menu_name'          => 'Slider Header Home', // название меню
         ),
-//        'description'         => '',
         'public'              => true,
-//        'publicly_queryable'  => null, // зависит от public
-//        'exclude_from_search' => null, // зависит от public
-//        'show_ui'             => null, // зависит от public
-//        'show_in_menu'        => null, // показывать ли в меню адмнки
-//        'show_in_admin_bar'   => null, // по умолчанию значение show_in_menu
-//        'show_in_nav_menus'   => null, // зависит от public
-//        'show_in_rest'        => null, // добавить в REST API. C WP 4.7
-//        'rest_base'           => null, // $post_type. C WP 4.7
-//        'menu_position'       => null,
-//        'menu_icon'           => null,
-        //'capability_type'   => 'post',
-        //'capabilities'      => 'post', // массив дополнительных прав для этого типа записи
-        //'map_meta_cap'      => null, // Ставим true чтобы включить дефолтный обработчик специальных прав
         'hierarchical'        => false,
         'supports'            => array('title', 'thumbnail', 'custom-fields'), // 'title','editor','author','thumbnail','excerpt','trackbacks','custom-fields','comments','revisions','page-attributes','post-formats'
+        'taxonomies'          => array(),
+        'has_archive'         => false,
+        'rewrite'             => true,
+        'query_var'           => true,
+    ) );
+}
+/*
+ * Register Custom Post Type Advantages Header Home
+ */
+add_action('init', 'bepro_register_post_type_advantages_header_home');
+function bepro_register_post_type_advantages_header_home(){
+        register_post_type('advantages-header', array(
+        'label'  => null,
+        'labels' => array(
+            'name'               => 'Our Advantages', // основное название для типа записи
+            'singular_name'      => 'Advantag', // название для одной записи этого типа
+            'add_new'            => 'Add Advantag', // для добавления новой записи
+            'add_new_item'       => 'Add', // заголовка у вновь создаваемой записи в админ-панели.
+            'edit_item'          => 'Write', // для редактирования типа записи
+            'new_item'           => 'New Advantag', // текст новой записи
+            'view_item'          => 'View', // для просмотра записи этого типа.
+            'search_items'       => 'Search', // для поиска по этим типам записи
+            'not_found'          => 'Not Find', // если в результате поиска ничего не было найдено
+            'not_found_in_trash' => 'Not find in trash', // если не было найдено в корзине
+            'parent_item_colon'  => '', // для родителей (у древовидных типов)
+            'menu_name'          => 'Our Advantages', // название меню
+        ),
+        'public'              => true,
+        'hierarchical'        => false,
+        'supports'            => array('title', 'custom-fields'), // 'title','editor','author','thumbnail','excerpt','trackbacks','custom-fields','comments','revisions','page-attributes','post-formats'
         'taxonomies'          => array(),
         'has_archive'         => false,
         'rewrite'             => true,
--- wp-content/themes/bepro/template-parts/content-slider-header-home.php
+++ wp-content/themes/bepro/template-parts/content-slider-header-home.php
@@ -1,3 +1,4 @@
+<!--Start Banner Home Page-->
 <section class="banner-home">
     <div id="bannerSliderHome" class="owl-carousel banner-home_slider">
         <?php
@@ -14,8 +15,8 @@
                     <div class="banner-home_content">
                         <h1><?php the_title(); ?></h1>
                         <?php $id = get_the_id(); ?>
-                        <?php $text_btn = get_post_meta($id, 'SliderHeaderHomeText', true) ? get_post_meta($id, 'SliderHeaderHomeText', true) : 'buy no!'?>
-                        <?php $url_btn = get_post_meta($id, 'SliderHeaderHomeUrl', true) ? get_post_meta($id, 'SliderHeaderHomeUrl', true) : '#'?>
+                        <?php $text_btn = get_post_meta($id, 'SliderHeaderHomeText', true) ? get_post_meta($id, 'SliderHeaderHomeText', true) : 'buy no!'; ?>
+                        <?php $url_btn = get_post_meta($id, 'SliderHeaderHomeUrl', true) ? get_post_meta($id, 'SliderHeaderHomeUrl', true) : '#'; ?>
                         <a href="<?php echo $url_btn; ?>" class="button button__big button__radius button__blue banner-home_button"><?php echo $text_btn; ?></a>
                     </div>
                 </div>
@@ -24,51 +25,48 @@
         }
         wp_reset_postdata();
         ?>
-    </div>
-    <div class="advantages">
-        <div class="container">
-            <div class="row">
-                <div class="col-12">
-                    <div class="advantages_outer">
-                        <div class="advantages_item">
-                            <div class="advantages_icon">
-                                <i class="icon-paper-plane"></i>
-                            </div>
-                            <div class="advantages_content">
-                                <h4>Free Shipping</h4>
-                                <p>Oders over $99</p>
-                            </div>
-                        </div>
-                        <div class="advantages_item">
-                            <div class="advantages_icon">
-                                <i class="icon-refresh"></i>
-                            </div>
-                            <div class="advantages_content">
-                                <h4>30 days return</h4>
-                                <p>If goods have problem</p>
-                            </div>
-                        </div>
-                        <div class="advantages_item">
-                            <div class="advantages_icon">
-                                <i class="icon-credit-card"></i>
-                            </div>
-                            <div class="advantages_content">
-                                <h4>Secure payment</h4>
-                                <p>100% secure payment</p>
-                            </div>
-                        </div>
-                        <div class="advantages_item">
-                            <div class="advantages_icon">
-                                <i class="icon-user"></i>
-                            </div>
-                            <div class="advantages_content">
-                                <h4>24h Support</h4>
-                                <p>Dedicated Support</p>
-                            </div>
-                        </div>
+    </div><!--End Banner Home Page-->
+    <!-- Start Our Advantages -->
+    <?php
+    $args = array(
+        'posts_per_page' => 4,
+        'post_type' => 'advantages-header',
+    );
+    $advantages = new WP_Query( $args );
+    if ( $advantages->have_posts() ) { ?>
+        <div class="advantages">
+            <div class="container">
+                <div class="row">
+                    <div class="col-12">
+                        <div class="advantages_outer">
+                        <?php while ( $advantages->have_posts() ) {
+                            $advantages->the_post(); ?>
+                                <?php $id = get_the_id(); ?>
+                                <div class="advantages_item">
+                                    <?php $icon = get_post_meta($id, 'advantagesIcon', true) ? get_post_meta($id, 'advantagesIcon', true) : ''; ?>
+                                    <?php if( $icon ) : ?>
+                                        <div class="advantages_icon">
+                                            <i class="<?php echo $icon; ?>"></i>
+                                        </div>
+                                    <?php endif; ?>
+                                    <div class="advantages_content">
+                                        <?php if( get_the_title() ) {
+                                            the_title('<h4>', '<h4>');
+                                        } ?>
+                                        <?php $text = get_post_meta($id, 'advantagesText', true) ? get_post_meta($id, 'advantagesText', true) : ''; ?>
+                                        <?php if( $text ) : ?>
+                                            <p><?php echo $text; ?></p>
+                                        <?php endif; ?>
+                                    </div>
+                                </div>
+                            <?php
+                        }
+                    }
+                    wp_reset_postdata();
+                    ?>
                     </div>
                 </div>
             </div>
         </div>
-    </div>
+    </div><!-- End Our Advantages -->
 </section>
