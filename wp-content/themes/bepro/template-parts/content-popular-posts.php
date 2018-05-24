8aa01d36df5e1f259834f53f8926684234ea5fc5
tshevchenko <tshevchenko@ro.ru>
2018-05-24 08:49:00 +0300

    'Created-the-structure-of-the-Slider-popular-posts-was-difficult-but-successful'

--- wp-content/themes/bepro/template-parts/content-popular-posts.php
+++ wp-content/themes/bepro/template-parts/content-popular-posts.php
@@ -16,41 +16,42 @@
                 </div>
                 <?php
                 $args = array(
-                    'posts_per_page' => 4,
+                    'posts_per_page' => 3,
                     'post_type' => 'post',
                 );
-                $blog_posts = new WP_Query( $args );
-                if ( $blog_posts->have_posts() ) { ?>
-                    <?php $this_post = 0; ?>
-                    <!-- Start Slider -->
+                $posts = get_posts( $args );
+                $item = 0;
+
+
+                wp_reset_postdata(); // сброс
+                ?>
+
+                   <!-- Start Slider -->
                     <div id="popularPostsSlider" class="popular-posts_slider">
-                        <?php while ( $blog_posts->have_posts() ) {
-                            $blog_posts->the_post(); ?>
-                                <?php if( $this_post === 0 ) : ?>
-                                <!--Start Item-->
-                                <div class="popular-posts_item">
-                                    <div class="popular-posts_item-big">
-
-                                    </div>
-                                <?php else : ?>
-                                    <div class="popular-posts_item-small">
-
-                                    </div>
-                                <?php endif; ?>
-                        <?php
-                            if ( $this_post === 2 ) {
-                                $this_post = 0;
-                            } else {
-                                $this_post++;
-                            }
-                            if( $this_post === 0 ) {
-                                echo '</div><!-- End Item -->';
-                            }
+                 <?php
+                    foreach($posts as $key => $post){ setup_postdata($post);
+                        if ( $item === 0 ) { // Open Item Slider ?>
+                            <!--Start Item-->
+                            <div class="popular-posts_item">
+                                <div class="popular-posts_left">
+                                    <div class="popular-posts_item-big"><?php echo $item; ?></div>
+                                </div><!-- Left -->
+                                <div class="popular-posts_right">
+                        <?php } else { ?>
+                                        <div class="popular-posts_item-small"><?php echo $item; ?></div>
+                        <?php } ?>
+                        <?php if ( !isset( $posts[++$key] ) || $item === 2 )  { // Close Item Slider
+                             ?> </div> <!--Right--> <?php
+                            $item = 0;
+                        ?>
+                            </div><!-- End Item -->
+                        <?php } else {
+                            $item++;
                         }
-                        echo '</div><!-- End Slider -->';
+
                     }
-                        wp_reset_postdata();
-                    ?>
+                 ?>
+                </div><!-- End Slider -->
             </div><!--Popular Posts-->
         </div><!--Col-->
     </div><!--Row-->
